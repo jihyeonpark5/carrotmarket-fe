5a9147ed0d0437ef9be8b171730ccf65793b982d
hansololiviakim <dev.hansolkim@gmail.com>
2023-05-12 16:02:06 +0900

    Chore: 프로젝트 기본 패키지 세팅

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Main from './pages/Main';
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <div>App</div>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Main />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   )
 }
 
