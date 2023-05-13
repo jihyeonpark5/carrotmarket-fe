0370710ad8c10666fc16ac05495eea683eabd0fc
hansololiviakim <dev.hansolkim@gmail.com>
2023-05-13 13:17:22 +0900

    [Feat] 게시글 상세 페이지 UI

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import SignUp from './SignUp'
 import Intro from './Intro'
 import MyPage from './MyPage';
 import Main from './Main';
+import BoardList from './BoardList';
 
-export {Login, SignUp, Intro, MyPage, Main};
+export { Login, SignUp, Intro, MyPage, Main, BoardList };
 
