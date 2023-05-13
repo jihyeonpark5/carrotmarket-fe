4aa43f85230ddfd5127740b7ec677e32d06bab9d
Hansol Olivia Kim <84097192+hansololiviakim@users.noreply.github.com>
2023-05-14 00:23:12 +0900

    Feat: 게시글 리스트, 상세 페이지 UI 추가 및 버튼, 이미지 UI 컴포넌트 수정 (#9)
    * [Feat] webkit scrollbar none 추가
    * [Feat] 글 작성 버튼 UI 추가
    * [Feat] 게시글 상세 페이지 UI 구현
    * [Feat] 거래상태값 버튼 컴포넌트 생성, 이미지 컴포넌트 코드 수정

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,8 @@ import MyPage from './MyPage';
 import Main from './Main';
 import Search from './Search'
 import BoardList from './BoardList';
+import BoardDetail from './BoardDetail'
 import ChatList from './ChatList'
 
-export { Login, SignUp, Intro, MyPage, Main, Search, BoardList, ChatList };
+export { Login, SignUp, Intro, MyPage, Main, Search, BoardList, BoardDetail, ChatList };
 
