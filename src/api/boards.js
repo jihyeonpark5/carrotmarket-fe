339de4ff3195a59eab68d7dbe28494e800523436
hansololiviakim <dev.hansolkim@gmail.com>
2023-05-17 02:01:35 +0900

    [Feat] 게시글 작성 기능

--- src/api/boards.js
+++ src/api/boards.js
@@ -1,6 +1,20 @@
 import axios from 'axios';
 import { instance } from './axios';
 
+// * 게시글 작성
+export const submitBoard = (boardFormData) => {
+  return instance.post('/api/board', boardFormData)
+  .then((response) => {
+    console.log('axios 게시글 작성 성공!');
+    return response.data;
+  })
+  .catch((error) => {
+    console.error(error.response.data);
+  })
+}
+
+
+// * 내 게시글 조회
 export const getBoard = ( access_token ) => {
     return instance.get('/api/myboard',{
         headers:{
--- src/pages/BoardWrite.jsx
+++ src/pages/BoardWrite.jsx
@@ -3,38 +3,98 @@ import { styled } from 'styled-components';
 import { Layout, Image, CommonButton } from '../components/ui';
 import { BsCameraFill } from 'react-icons/bs';
 import { AiFillMinusCircle } from 'react-icons/ai';
+import { useMutation } from 'react-query';
+import { submitBoard } from '../api/boards';
+
+// TODO 2023-05-17 로그인 pull 받고 업로드 다시 확인해볼 것
 
 // ! 글 작성하기, 수정하기 모두 해당 페이지에서 진행
 function BoardWrite() {
+  const [title, setTitle] = useState('');
+  const [price, setPrice] = useState('');
+  const [content, setContent] = useState('');
   const [preview, setPreview] = useState('');
+  const [file, setFile] = useState('');
 
   // * 썸네일 업로드
-  const fileChangeHandler = (e) => {
-    const file = e.target.files[0];
-    if (!file) return;
-    if (file.type !== 'image/jpeg' && file.type !== 'image/png') {
+  const onFileChange = (e) => {
+    const newFile = e.target.files[0];
+    if (!newFile) return;
+    if (newFile.type !== 'image/jpeg' && newFile.type !== 'image/png') {
       alert('jpg, png 형식의 이미지 파일을 업로드해주세요.');
       return;
     } else {
-      const previewURL = window.URL.createObjectURL(file);
-      setPreview(previewURL)
+      const previewURL = window.URL.createObjectURL(newFile);
+      setPreview(previewURL);
+      setFile(newFile);
     }
   }
 
   // * 썸네일 삭제
-  const onDeleteThumbnail = () => {
+  const onFileDelete = () => {
     const deleteConfirm = window.confirm('업로드한 이미지를 삭제하시겠습니까?');
     if (deleteConfirm) setPreview('');
   }
 
+  // * 제목 입력값 감지
+  const onTitleChange = (e) => {
+    if (e.target.value.length > 30) {
+      alert('제목은 30자 이상 입력할 수 없습니다.');
+      return;
+    }
+    setTitle(e.target.value);
+  }
+
+  // * 가격 입력값 감지
+  const onPriceChange = (e) => {
+    const newPrice = e.target.value.replace(/\D/g, '');
+    if (newPrice === '') {
+      setPrice('');
+    } else {
+      setPrice(Number(newPrice).toLocaleString());
+    }
+  }
+
+  // * 내용 입력값 감지
+  const onContentChange = (e) => {
+    setContent(e.target.value);
+  }
+
+  // * 게시글 작성 버튼 클릭
+  const onSubmitClick = (e) => {
+    e.preventDefault();
+    if (title === '' || price === '' || content === '' || file === '') {
+      alert('모든 내용을 입력해주세요.');
+      return;
+    }
+
+    const boardFormData = new FormData();
+    boardFormData.append('title', title);
+    boardFormData.append('image', file);
+    boardFormData.append('content', content.replaceAll(/\n/g, '<br>'));
+    boardFormData.append('price', price.replaceAll(',', ''));
+
+    submitBoardMutaion.mutate(boardFormData);
+  }
+
+  // * 게시글 작성 useMutation
+  const submitBoardMutaion = useMutation(submitBoard, {
+    onSuccess: (response) => {
+      // console.log('onSuccess', response);
+    }
+  })
+
   return (
     <Layout>
       {/* 뒤로가기 버튼 클릭 && URL이 글 작성일 경우 뒤로 갈건지 확인 alert */}
-      <ContentForm>
+      <ContentForm
+        method="post"
+        encType="multipart/form-data"
+      >
         <SetImgDiv>
             <label>
               <BsCameraFill />
-              <input type="file" name="image" onChange={fileChangeHandler} />
+              <input type="file" name="image" onChange={onFileChange} />
             </label>
             {
               preview !== '' &&
@@ -47,7 +107,7 @@ function BoardWrite() {
                   alt={'썸네일 이미지'}
                 />
                 <StyledMinusCircle
-                  onClick={onDeleteThumbnail}
+                  onClick={onFileDelete}
                 />
               </>
             }
@@ -59,6 +119,8 @@ function BoardWrite() {
               type="text"
               id="title"
               name="title"
+              value={title}
+              onChange={onTitleChange}
             />
           </SetInfo>
           <SetInfo>
@@ -67,17 +129,28 @@ function BoardWrite() {
               type="text"
               id="price"
               name="price"
+              value={price}
+              onChange={onPriceChange}
             />
           </SetInfo>
           <BoardLabel htmlFor="content" />
           <SetInfo>
             <SetContent
+              id="content"
+              name="content"
+              value={content}
+              onChange={onContentChange}
               placeholder="역삼동에 올릴 게시글 내용을 작성해주세요."
             />
           </SetInfo>
         </SetBoardDiv>
         {/* 수정하기 클릭해 진입했을 경우 글 수정하기로 출력 */}
-        <CommonButton size={'large'}>글 작성하기</CommonButton>
+        <CommonButton
+          size={'large'}
+          onClick={onSubmitClick}
+        >
+          글 작성하기
+        </CommonButton>
       </ContentForm>
     </Layout>
   )
