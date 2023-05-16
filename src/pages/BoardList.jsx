19512bb82164f7246cd6ace73781fab5e1385904
hansololiviakim <dev.hansolkim@gmail.com>
2023-05-17 04:18:30 +0900

    [Feat] 게시글 리스트 조회 기능

--- src/pages/BoardList.jsx
+++ src/pages/BoardList.jsx
@@ -1,112 +1,73 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { styled } from 'styled-components';
 import { Layout, Image, StatusButton } from '../components/ui';
 import { SlArrowDown } from 'react-icons/sl';
-// * 이미지 임시
-import carrot from '../assets/dangeunee_test_img.png';
+import { useMutation } from 'react-query';
+import { Link } from 'react-router-dom';
+import { getBoards } from '../api/boards';
 
 function BoardList() {
+  const [boardData, setBoardData] = useState(null);
+
+  // * 페이지가 마운트될 때 게시글 리스트를 조회하도록 설정
+  useEffect(() => {
+    getBoardList();
+  }, []);
+
+  // * 게시글 리스트 조회
+  const getBoardList = () => {
+    const setPage = {
+      "page": 0,
+      "size": 1,
+      "sort": ["createdAt,DESC"],
+    }
+    getBoardListMutation.mutate(setPage);
+  }
+
+  // * 게시글 리스트 조회 useMutation
+  const getBoardListMutation = useMutation(getBoards, {
+    onSuccess: (response) => {
+      setBoardData(response.data.responseDtos);
+    }
+  })
+
   return (
     <Layout>
       <ListNav>
+        {/* TODO 사용자의 동 이름으로 출력해야 함 */}
         <span>문학동</span>
         <SlArrowDown />
       </ListNav>
       <ListSection>
-        <ListOneDiv>
-          <Image
-            width={'130px'}
-            height={'130px'}
-            borderradius={'10px'}
-            src={carrot}
-            alt={'상품 이미지'}
-          />
-          <ListInfoDiv>
-            <ListTitleH1>애플 에어팟 3세대 미개봉</ListTitleH1>
-            <ListDetailH3>
-              <span>미추홀구 학익동</span>
-              <span>·</span>
-              <span>37분 전</span>
-            </ListDetailH3>
-            <ListPriceH2>210,000원</ListPriceH2>
-          </ListInfoDiv>
-        </ListOneDiv>
-        <ListOneDiv>
-          <Image
-            width={'130px'}
-            height={'130px'}
-            borderradius={'10px'}
-            src={carrot}
-            alt={'상품 이미지'}
-          />
-          <ListInfoDiv>
-            <ListTitleH1>애플 에어팟 3세대 미개봉</ListTitleH1>
-            <ListDetailH3>
-              <span>미추홀구 학익동</span>
-              <span>·</span>
-              <span>37분 전</span>
-            </ListDetailH3>
-            <ListPriceH2>
-              <StatusButton color={'black'}>거래완료</StatusButton>
-              <span>210,000원</span>
-            </ListPriceH2>
-          </ListInfoDiv>
-        </ListOneDiv>
-        <ListOneDiv>
-          <Image
-            width={'130px'}
-            height={'130px'}
-            borderradius={'10px'}
-            src={carrot}
-            alt={'상품 이미지'}
-          />
-          <ListInfoDiv>
-            <ListTitleH1>애플 에어팟 3세대 미개봉</ListTitleH1>
-            <ListDetailH3>
-              <span>미추홀구 학익동</span>
-              <span>·</span>
-              <span>37분 전</span>
-            </ListDetailH3>
-            <ListPriceH2>210,000원</ListPriceH2>
-          </ListInfoDiv>
-        </ListOneDiv>
-        <ListOneDiv>
-          <Image
-            width={'130px'}
-            height={'130px'}
-            borderradius={'10px'}
-            src={carrot}
-            alt={'상품 이미지'}
-          />
-          <ListInfoDiv>
-            <ListTitleH1>애플 에어팟 3세대 미개봉</ListTitleH1>
-            <ListDetailH3>
-              <span>미추홀구 학익동</span>
-              <span>·</span>
-              <span>37분 전</span>
-            </ListDetailH3>
-            <ListPriceH2>210,000원</ListPriceH2>
-          </ListInfoDiv>
-        </ListOneDiv>
-        <ListOneDiv>
-          <Image
-            width={'130px'}
-            height={'130px'}
-            borderradius={'10px'}
-            src={carrot}
-            alt={'상품 이미지'}
-          />
-          <ListInfoDiv>
-            <ListTitleH1>애플 에어팟 3세대 미개봉</ListTitleH1>
-            <ListDetailH3>
-              <span>미추홀구 학익동</span>
-              <span>·</span>
-              <span>37분 전</span>
-            </ListDetailH3>
-            <ListPriceH2>210,000원</ListPriceH2>
-          </ListInfoDiv>
-        </ListOneDiv>
-        <WriteButton>+</WriteButton>
+        {
+          !!boardData &&
+          boardData.map((board) => {
+            <ListOneDiv key={board.id}>
+              <Image
+                width={'130px'}
+                height={'130px'}
+                borderradius={'10px'}
+                src={board.image}
+                alt={'상품 이미지'}
+              />
+              <ListInfoDiv>
+                <ListTitleH1>{board.title}</ListTitleH1>
+                <ListDetailH3>
+                  <span>{board.address}</span>
+                </ListDetailH3>
+                <ListPriceH2>
+                  {/* TODO 둘 다 잘 나오는지 확인 */}
+                  {/* {board.status && <StatusButton color={'black'}>거래완료</StatusButton>} */}
+                  {!!board.status ? <StatusButton color={'black'}>거래완료</StatusButton> : ''}
+                  {Number(board.price).toLocaleString()}원
+                </ListPriceH2>
+              </ListInfoDiv>
+            </ListOneDiv>
+          })
+        }
+        <WriteButton>
+          <Link to="/BoardWrite">+</Link>
+        </WriteButton>
       </ListSection>
     </Layout>
   )
@@ -135,6 +96,7 @@ const ListNav = styled.nav`
 
 const ListSection = styled.section`
   display: flex;
+  position: relative;
   padding-top: 60px;
   flex-direction: column;
 `
@@ -178,9 +140,9 @@ const ListDetailH3 = styled.h3`
 const WriteButton = styled.button`
   width: 60px;
   height: 60px;
-  position: fixed;
-  top: 810px;
-  right: 750px;
+  position: absolute;
+  top: 740px;
+  right: 20px;
   border: none;
   border-radius: 50%;
   background-color: #FF7E36;
