4ad09b6f50969a90d795702b0a5d58318ef5a155
parrrk <sohee1422@dongguk.edu>
2024-10-28 18:43:59 +0900

    Change Header

--- src/components/element/Header.jsx
+++ src/components/element/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link, useNavigate } from 'react-router-dom';
 import { styled } from 'styled-components';
-import logo from "../../assets/Daangeun_Logo_Color_RGB.svg"
+import logo from "../../assets/soso_logo.svg"
 import { SlArrowLeft } from "react-icons/sl";
 
 function Header({children}) {
@@ -10,7 +10,7 @@ function Header({children}) {
     <StHeader>
         <button type='button' onClick={() => navigate(-1)}><SlArrowLeft /></button>
         {children}
-        <Link to='/BoardList' title="메인페이지로 이동"><img src={logo} alt='당근마켓로고'/></Link>
+        <Link to='/BoardList' title="메인페이지로 이동"><img src={logo} alt='소소하게로고'/></Link>
     </StHeader>
   )
 }
@@ -19,29 +19,35 @@ export default Header;
 
 const StHeader = styled.header`
     position: sticky;
-    top:0;
-    z-index:10;
-    width:100%;
-    height:55px;
-    background-color:#fff;
-    box-shadow: rgba(0, 0, 0, 0.1) 0 5px 6px -1px ;
-    & button{
-        position:absolute;
-        top:50%;
-        left:20px;
-        transform:translateY(-50%);
-        border:none;
-        background-color:transparent;
-        font-size:22px;
-        color:#777;
+    top: 0;
+    z-index: 10;
+    width: 100%;
+    height: 55px;
+    background-color: #fff;
+    box-shadow: rgba(0, 0, 0, 0.1) 0 5px 6px -1px;
+    display: flex; /* Flexbox 사용 */
+    align-items: center; /* 수직 중앙 정렬 */
+    justify-content: space-between; /* 좌우 간격 조정 */
+    
+    & button {
+        position: absolute;
+        top: 50%;
+        left: 20px;
+        transform: translateY(-50%);
+        border: none;
+        background-color: transparent;
+        font-size: 22px;
+        color: #777;
     }
-    & > a{
-        display:block;
-        width:150px;
+
+    & > a {
+        display: block;
+        width: 150px;
+        margin: 0 auto; /* 가운데 정렬 */
+    }
+
+    & > a > img {
+        width: 120px;
         margin: 0 auto;
-    }
-    & > a > img{
-        width:130px;
-        margin:0 auto;
     }
 `
